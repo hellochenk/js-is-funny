98ac1b649d88ead78fe6b7c96c1ddd09d80e6fd8
hellochenk <657301050@qq.com>
2018-06-05 00:17:38 +0800

    add nodemon

--- src/server/app.js
+++ src/server/app.js
@@ -2,11 +2,14 @@ const Koa = require('koa');
 const app = new Koa();
 const port = 8008;
 
-app.use(async(ctx, next) => {
+const home = async (ctx, next)=>{
     await next();
     ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
-});
+    ctx.response.body = '<h2>Hello, koa2!</h2>';
+}
+
+app.use(home);
+
 // 在端口3000监听:
 app.listen(port);
 console.log(`app started at port ${port}...`);
