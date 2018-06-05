50bc699192bd8ef378d358ddf1f031189825cce2
hellochenk <657301050@qq.com>
2018-06-06 00:24:12 +0800

    add msg

--- src/server/app.js
+++ src/server/app.js
@@ -2,14 +2,28 @@ const Koa = require('koa');
 const app = new Koa();
 const port = 8008;
 
-const home = async (ctx, next)=>{
+const date = async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+}
+const logger = async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+}
+const home = async (ctx, next) => {
     await next();
     ctx.response.type = 'text/html';
     ctx.response.body = '<h2>Hello, koa2!</h2>';
 }
 
+app.use(date);
+app.use(logger);
 app.use(home);
-
 // 在端口port监听:
+
 app.listen(port);
 console.log(`app started at port ${port}...`);
