cddebefed8c89bc0c8bd3e073cfd3f69616a2272
chenk <657301050@qq.com>
2018-06-11 16:00:40 +0800

    change config

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,21 +1,11 @@
-import Koa =  require('koa');
-import Test from './initRouter';
-
-let p = new Test('123');
-console.log(p.getVal());
-
-function greeter(person: string) {
-  return "Hello, " + person;
-}
-let a:string
-let user = "Jane User";
-a = user
+import * as Koa from 'koa';
 let port:number = 9990;
 
 let app = new Koa();
 app.use((ctx:any, next:any)=>{
   ctx.body = 'use ts in koa'
 })
+
 app.listen(port, '0.0.0.0', () => {
   console.log(`app started at port ${port}...`)
 })
