01f0eeb60527b3065d44c0dcf842001975e22533
chenk <657301050@qq.com>
2018-07-27 18:39:44 +0800

    start rx

--- src/client/modules/testrx/index.jsx
+++ src/client/modules/testrx/index.jsx
@@ -2,20 +2,16 @@ import * as Rx from 'rxjs/Rx';
 import { BaseService } from '../../service/service'
 const service = new BaseService()
 
-var foo = Rx.Observable.create(function (observer) {
-  console.log('Hello');
-  observer.next(42);
-});
+// 
+let fetch = Rx.Observable.create(async oberver => {
+  let resp = await service.request('search')
+  oberver.next(resp)
+})
 
-foo.subscribe(function (x) {
-  console.log(x);
-});
-foo.subscribe(function (y) {
-  console.log(y);
-});
-
-console.log('before');
-console.log(foo.call());
-console.log('after');
+fetch.subscribe({
+  next: (resp) =>{
+    console.log(resp)
+  }
+})
 
 export default () => (123)
