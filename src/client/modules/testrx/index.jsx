bb7e3a9d5496f252560c64d962ff7eb6f52ad86a
chenk <657301050@qq.com>
2018-08-09 18:43:01 +0800

    merge code

--- src/client/modules/testrx/index.jsx
+++ src/client/modules/testrx/index.jsx
@@ -1,18 +1,27 @@
 import * as Rx from 'rxjs/Rx';
 import { BaseService } from '../../service/service'
 // const service = new BaseService()
+// rx must have data souces --->
 
 let subject = new Rx.Subject()
+var source = Rx.Observable.from([1, 2, 3]);
 
-subject.subscribe({
-  next: val => console.log('next: ', val),
-  error: err => console.log('error:', err),
-  complete: () => console.log('finished')
+// source 是observerable对象，调用multicast 传入subject，订阅信息, 返回一个类似observerable对象
+// multicast 操作符 
+var multicasted = source.multicast(subject);
+// source.multicast(submsgb);
+
+
+multicasted.subscribe({
+  next: val => console.log('a', val)
 })
 
-subject.next(1)
-subject.next(2)
-subject.next(3)
-subject.complete(1)
+multicasted.subscribe({
+  next: val => console.log('b', val)
+})
+
+multicasted.connect()
+
+
 
 export default () => (123)
--- src/server/controllers/index.controller.ts
+++ src/server/controllers/index.controller.ts
@@ -1,4 +1,14 @@
+
+
 const getIndex = async (ctx:any) => {
+  await fetch('localhost:9991/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
+
   ctx.response.body = 123
 }
 
