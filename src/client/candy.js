b31f4be52caf930af37766bbca85ba8863cd8e82
chenk <657301050@qq.com>
2018-08-10 18:17:24 +0800

    merge code

--- src/client/candy.js
+++ src/client/candy.js
@@ -1,1 +1,7 @@
 import 'babel-polyfill';
+import * as Rx from 'rxjs/Rx';
+import * as _ from 'lodash';
+if (process.env.NODE_ENV !== 'production') {
+  window.Rx = Rx
+  window._ = _
+}
--- src/client/modules/testrx/index.jsx
+++ src/client/modules/testrx/index.jsx
@@ -1,27 +1,9 @@
+import React from 'react'
 import * as Rx from 'rxjs/Rx';
+import * as _ from 'lodash'
 import { BaseService } from '../../service/service'
-// const service = new BaseService()
-// rx must have data souces --->
+const service = new BaseService()
 
-let subject = new Rx.Subject()
-var source = Rx.Observable.from([1, 2, 3]);
+console.log(1)
 
-// source 是observerable对象，调用multicast 传入subject，订阅信息, 返回一个类似observerable对象
-// multicast 操作符 
-var multicasted = source.multicast(subject);
-// source.multicast(submsgb);
-
-
-multicasted.subscribe({
-  next: val => console.log('a', val)
-})
-
-multicasted.subscribe({
-  next: val => console.log('b', val)
-})
-
-multicasted.connect()
-
-
-
-export default () => (123)
+export default () => <p style={{padding: 10}}>let's test rxjs & lodash</p>
